b2f5e8be451dc1e99768df61c8b2727217270218
snchat78-svg <snchat78@gmail.com>
2026-08-07 16:08:08 +0530

    Implement Brain class with command logging

--- ai_core/brain.py
+++ ai_core/brain.py
@@ -1,1 +1,15 @@
+"""
+Project : Vyom AI
+Version : 0.1
+Module  : Brain
+"""
 
+from ai_core.logger import log
+
+class Brain:
+
+    def think(self, command):
+
+        log(f"Received Command : {command}")
+
+        return f"Command Accepted : {command}"
