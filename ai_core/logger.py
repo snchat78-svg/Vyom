c812795f32b0949a306fb9a1d1563cad133e097a
snchat78-svg <snchat78@gmail.com>
2026-08-07 16:07:35 +0530

    Add logger module with timestamped log function

--- ai_core/logger.py
+++ ai_core/logger.py
@@ -1,1 +1,11 @@
+"""
+Project : Vyom AI
+Version : 0.1
+Module  : Logger
+"""
 
+from datetime import datetime
+
+def log(message):
+    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"[{time}] {message}")
