8234cca4e9b8556286143df129c8797d42eddab0
snchat78-svg <snchat78@gmail.com>
2026-08-07 16:15:39 +0530

    Update executor.py

--- command_engine/executor.py
+++ command_engine/executor.py
@@ -1,13 +1,17 @@
 """
 Project : Vyom AI
-Version : 0.1
+Version : 0.2
 Module  : Executor
 """
 
 from ai_core.brain import Brain
+from command_engine.intent import IntentEngine
 
 brain = Brain()
+intent_engine = IntentEngine()
 
 def execute(command):
 
-    return brain.think(command)
+    intent = intent_engine.detect(command)
+
+    return brain.think(intent)
