85a893371bd290322ea64fb3453174ae8d2491e8
snchat78-svg <snchat78@gmail.com>
2026-08-10 16:07:48 +0530

    Update version number in executor module
    Updated version number from 0.6 to 0.8 in the executor module.

--- command_engine/executor.py
+++ command_engine/executor.py
@@ -1,12 +1,13 @@
 """
 Project : Vyom AI
-Version : 0.6
-Module  : Executor
+Version : 0.8
+Module : Executor
 """
 
 from ai_core.brain import Brain
 from command_engine.intent import IntentEngine
 from tools.tool_manager import ToolManager
+
 
 brain = Brain()
 intent_engine = IntentEngine()
