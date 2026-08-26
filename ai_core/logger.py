03709ec8a5352c1f0d3534d4623ab2a44e128c0c
snchat78-svg <snchat78@gmail.com>
2026-08-26 11:34:42 +0530

    Update version to 0.2 and add flush to log

--- ai_core/logger.py
+++ ai_core/logger.py
@@ -1,11 +1,12 @@
 """
 Project : Vyom AI
-Version : 0.1
+Version : 0.2
 Module  : Logger
 """
 
 from datetime import datetime
 
+
 def log(message):
     time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"[{time}] {message}")
+    print(f"[{time}] {message}", flush=True)
